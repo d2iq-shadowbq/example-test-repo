69d24718bda014f3d0dde4b18fa0739bc80f885b
shadowbq <shadowbq@gmail.com>
2020-02-24 13:49:29 -0500

    build 0.1.0-rc1

--- src/no-op.c
+++ src/no-op.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <inttypes.h>
 int main() { 
-    int64_t epoch = 1582570125;
+    int64_t epoch = 1582570169;
     printf("Epoch Compiled: %"PRId64"\n",epoch);
     return 0; 
     }
